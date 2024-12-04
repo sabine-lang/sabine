c918f500346f245f8c02201ce06be30c5c9c60a3
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-12-04 18:26:48 +0500

    chore: implemented identifier keywords

--- src/lexer.c
+++ src/lexer.c
@@ -240,6 +240,33 @@ bool lex_is_in_expression()
   return lex_process->current_expression_count > 0;
 }
 
+bool is_keyword(const char *str)
+{
+  return
+    // Types (default)
+    S_EQ(str, "unsigned") || S_EQ(str, "signed") || S_EQ(str, "char") ||
+    S_EQ(str, "short") || S_EQ(str, "int") || S_EQ(str, "long") ||
+    S_EQ(str, "float") || S_EQ(str, "double") || S_EQ(str, "void") ||
+    S_EQ(str, "struct") || S_EQ(str, "union") || S_EQ(str, "static") ||
+    S_EQ(str, "const") || S_EQ(str, "enum") ||
+
+    // Cancer (maybe)
+    S_EQ(str, "i8") || S_EQ(str, "i8") || S_EQ(str, "i16") ||
+    S_EQ(str, "i32") || S_EQ(str, "i64") || S_EQ(str, "i128") ||
+    S_EQ(str, "isize") || S_EQ(str, "u8") || S_EQ(str, "u16") ||
+    S_EQ(str, "u32") || S_EQ(str, "u64") || S_EQ(str, "u128") ||
+    S_EQ(str, "usize") ||
+
+    // Statements
+    S_EQ(str, "__ignore_typecheck") || S_EQ(str, "return") ||
+    S_EQ(str, "include") || S_EQ(str, "sizeof") || S_EQ(str, "if") ||
+    S_EQ(str, "else") || S_EQ(str, "while") || S_EQ(str, "for") ||
+    S_EQ(str, "do") || S_EQ(str, "break") || S_EQ(str, "continue") ||
+    S_EQ(str, "switch") || S_EQ(str, "case") || S_EQ(str, "default") ||
+    S_EQ(str, "goto") || S_EQ(str, "typedef") || S_EQ(str, "extern") ||
+    S_EQ(str, "restrict");
+}
+
 static struct token *token_make_operator_or_string()
 {
   // dedicated to every key newline include psychos
@@ -287,7 +314,10 @@ static struct token *token_make_identifier_or_keyword()
   // Null terminator
   buffer_write(buffer, 0x00);
 
-  // TODO: Keyword checker
+  if (is_keyword(buffer_ptr(buffer))) {
+    return token_create(
+      &(struct token){.type = TOKEN_TYPE_KEYWORD, .sval = buffer_ptr(buffer)});
+  }
 
   return token_create(
     &(struct token){.type = TOKEN_TYPE_IDENTIFIER, .sval = buffer_ptr(buffer)});
